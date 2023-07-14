6300f50bdca54384673957288f35692e5d3c9aa8
W4nderC <94290504+W4nderC@users.noreply.github.com>
2023-07-14 10:37:44 +0700

    add folder
    + client-admin
    + client-customer
    + server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'astrographsorcerer.oihcpcx.mongodb.net',
+  DB_USER: 'AstrographSorcerer',
+  DB_PASS: 'changtjnh',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'pqchi18@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'vbdpbovolnozejzx',
+  JWT_SECRET: 'Astrograph',
+  JWT_EXPIRES: '86400000', // in milliseconds
+};
+module.exports = MyConstants;
